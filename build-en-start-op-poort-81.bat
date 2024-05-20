2f25f6fd70a021b30a78307fe3ff09830b52b67e
ARWLND <arnout.rowland@student.hogent.be>
2024-05-20 21:59:19 +0200

    fixed mistakes

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Stop de bestaande container met de naam "demo-container", als deze actief is
+docker stop demo-container
+
+# Verwijder de bestaande container met de naam "demo-container", als deze bestaat
+docker rm demo-container
+
 # Bouw de Docker-image met de tag "demo-site"
 docker build -t demo-site .
 
-# Start een container met de naam "demo-container" op poort 81
+# Start een nieuwe container met de naam "demo-container" op poort 81
 docker run -d -p 81:81 --name demo-container demo-site
